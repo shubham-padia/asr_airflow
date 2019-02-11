dc92ea4f4f548070ec8bca2b99e2aca30d10eede
Shubham Padia <shubhamapadia@gmail.com>
2019-02-11 15:15:39 +0800

    metadata_registry: Add created_at columns to the table.

--- models/metadata_registry.py
+++ models/metadata_registry.py
@@ -1,6 +1,8 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, Boolean, String
+from sqlalchemy import Column, Integer, Boolean, String, DateTime
+from sqlalchemy.sql import func
 from models.base import Base
+import datetime
 
 class MetadataRegistry(Base):
     __tablename__ = 'metadata_registry'
@@ -8,7 +10,12 @@ class MetadataRegistry(Base):
     id = Column(Integer, primary_key=True)
     filename = Column(String)
     status = Column(Boolean(name='ck_status_bool'))
-
+    
+    # Make sure you aren't using default=datetime.datetime.utcnow(); you want
+    # to pass the utcnow function, not the result of evaluating it at module
+    # load.
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    
     def __init__(self, filename, status=False):
         self.filename = filename
         self.status = status
