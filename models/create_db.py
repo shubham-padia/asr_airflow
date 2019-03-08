b40de48bb8c354f2bb58f9d7ab034119a379e906
Shubham Padia <shubhamapadia@gmail.com>
2019-03-08 14:03:09 +0800

    db: Stamp head after creating the database.

--- models/create_db.py
+++ models/create_db.py
@@ -2,3 +2,8 @@ from models.base import Base, engine
 from models.metadata_registry import MetadataRegistry
 
 print(Base.metadata.create_all(engine))
+
+from alembic.config import Config
+from alembic import command
+alembic_cfg = Config("alembic.ini")
+command.stamp(alembic_cfg, "head")
