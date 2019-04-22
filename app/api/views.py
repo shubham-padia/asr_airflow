fb9a9528304bf09eba3f44d4c60dcd9cedd2d9fa
Shubham Padia <shubhamapadia@gmail.com>
2019-04-22 14:25:43 +0800

    Allow creating an entry in the metadata registry

--- app/api/views.py
+++ app/api/views.py
@@ -2,10 +2,10 @@ from rest_framework import generics
 from .models import MetadataRegistry
 from .serializers import MetadataRegistrySerializer
 
+class ListMetadataRegistry(generics.ListCreateAPIView):
+    """
+    Create an entry in the metadata registry or get a list of all the entries.
+    """
 
-class ListMetadataRegistry(generics.ListAPIView):
-    """
-    Provides a get method handler.
-    """
     queryset = MetadataRegistry.objects.all()
     serializer_class = MetadataRegistrySerializer
