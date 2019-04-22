bde5638c99611c9aa80b2f490ca70107cec09034
Shubham Padia <shubhamapadia@gmail.com>
2019-04-22 16:34:28 +0800

    Make fields required in serializer

--- app/api/models.py
+++ app/api/models.py
@@ -8,14 +8,13 @@ class MetadataRegistry(models.Model):
     class Meta:
         db_table = 'metadata_registry'
     
-    recording_id = models.CharField(max_length=256)
-    pipeline_id = models.CharField(max_length=256)
+    recording_id = models.CharField(max_length=256, null=False, blank=False)
+    pipeline_id = models.CharField(max_length=256, null=False, blank=False)
     status = models.BooleanField(default=False)
-    version = models.CharField(max_length=256)
+    version = models.CharField(max_length=256, null=False, blank=False)
     # Make sure you aren't using default=datetime.datetime.utcnow(); you want
     # to pass the utcnow function, not the result of evaluating it at module
     # load.
     created_at = models.DateTimeField(default=datetime.datetime.utcnow)
 
     pipeline = JSONField()
-
--- app/api/serializers.py
+++ app/api/serializers.py
@@ -7,6 +7,12 @@ class MetadataRegistrySerializer(serializers.ModelSerializer):
         model = MetadataRegistry
         fields = ("pipeline", "status", "version", "created_at")
         read_only_fields = ('created_at',)
+        extra_kwargs = {
+            "pipeline": {"required": True},
+            "status": {"required": True},
+            "version": {"required": True},
+            "created_at": {"required": True}
+        }
 
     def create(self, validated_data):
         return MetadataRegistry.objects.create(**validated_data)
