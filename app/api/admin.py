ffb931a0a6c966a7b1235faa298d4980eb2ee042
Shubham Padia <shubhamapadia@gmail.com>
2019-04-22 15:49:05 +0800

    Add recording_id and pipeline_id to the admin interface.

--- app/api/admin.py
+++ app/api/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import MetadataRegistry
 
 class MetadataRegistryAdmin(admin.ModelAdmin):
-    list_display = ['version', 'created_at', 'status']
-    list_filter = ('version', 'created_at', 'status') 
+    list_display = ['recording_id', 'pipeline_id', 'version', 'created_at', 'status']
+    list_filter = ('recording_id', 'pipeline_id', 'version', 'created_at', 'status') 
 
 admin.site.register(MetadataRegistry, MetadataRegistryAdmin)
