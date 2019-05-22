b6531135f2e0f49673f1502252d5911f856ebca2
Shubham Padia <shubhamapadia@gmail.com>
2019-05-22 14:10:17 +0800

    fixxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx

--- tools/fix_env.sh
+++ tools/fix_env.sh
@@ -1,5 +1,6 @@
 cp env-example .env
 sed -i -e "s/shubham/$USER/g" .env
+sed -i -e "s@CURRENT_DIR@$(pwd)@g" .env
 IP_ARRAY=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 IP=$(echo $IP_ARRAY | awk -F\  '{print $NF}')
 sed -i -e "s/server_domain_or_IP/$IP/g" .env
