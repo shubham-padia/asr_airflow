1e095c5a533a325321533b5ef998d5ac0ca9675a
Shubham Padia <shubhamapadia@gmail.com>
2019-05-23 16:18:52 +0800

    remove nginx

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -1,5 +1,3 @@
 sudo systemctl restart airflow-webserver
 sudo systemctl restart airflow-scheduler
-sudo systemctl restart gunicorn.socket
 sudo systemctl restart gunicorn
-sudo systemctl restart nginx
--- tools/setup_services.sh
+++ tools/setup_services.sh
@@ -5,9 +5,11 @@ sed -i -e "s/shubham/$USER/g" services/airflow-scheduler.service
 sed -i -e "s@CURRENT_DIR@$(pwd)@g" services/airflow-scheduler.service
 sed -i -e "s/shubham/$USER/g" services/gunicorn.service
 sed -i -e "s@CURRENT_DIR@$(pwd)@g" services/gunicorn.service
+IP_ARRAY=$(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
+IP=$(echo $IP_ARRAY | awk -F\  '{print $NF}')
+sed -i -e "s/server_domain_or_IP/$IP/g" services/gunicorn.service
 sudo cp services/* /lib/systemd/system
 sudo systemctl daemon-reload
 sudo systemctl enable gunicorn
 sudo systemctl enable airflow-webserver
 sudo systemctl enable airflow-scheduler
-sudo systemctl enable gunicorn.socket
