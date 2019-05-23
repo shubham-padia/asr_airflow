80c413d0109917ca6b1453beba41fbe17478c93f
Shubham Padia <shubhamapadia@gmail.com>
2019-05-23 11:26:07 +0800

    custom error messages

--- app/api/forms.py
+++ app/api/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class UploadAudioForm(forms.Form):
-    recording_id = forms.CharField(max_length=255)
-    file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+    recording_id = forms.CharField(max_length=255, error_messages={'required': 'Please enter the recording id'})
+    file = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}), error_messages={'required': 'Please upload atleast one file.'})
