f9cd38227ea0a27922f7747f39ab14465c43f9e9
Shubham Padia <shubhamapadia@gmail.com>
2019-05-28 14:09:39 +0800

    Do not setup nginx

--- install.sh
+++ install.sh
@@ -5,5 +5,4 @@ bash tools/fix_env.sh
 bash tools/setup_services.sh
 sudo su -c "bash $(pwd)/tools/install_db.sh" - postgres
 bash tools/setup_airflow.sh
-bash tools/setup_nginx.sh
 bash tools/setup_app.sh
