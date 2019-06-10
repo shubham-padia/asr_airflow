5c1e1ccf543d682fbd60662e1a435a4393ce015e
Shubham Padia <shubhamapadia@gmail.com>
2019-06-10 16:06:20 +0800

    install script changes

--- tools/install_pyenv.sh
+++ tools/install_pyenv.sh
@@ -1,3 +1,7 @@
 curl https://pyenv.run | bash
 pyenv install 3.7.2
 pyenv virtualenv 3.7.2 asr_airflow
+echo "export PATH=\"${PYENV_ROOT}/bin:\$PATH\"" >> ~/.bashrc
+echo "eval \"\$(pyenv init -)\""  >> ~/.bashrc
+echo "eval \"\$(pyenv virtualenv-init -)\""  >> ~/.bashrc
+source ~/.bashrc
