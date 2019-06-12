b8e30fb12789189b7e16be6a64f8bdf71940bdba
shubham-padia <shubhamapadia@gmail.com>
2019-06-12 13:44:59 +0800

    Install python2 dependencies

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ bash tools/setup_services.sh
 sudo su -c "bash $(pwd)/tools/install_db.sh" - postgres
 bash tools/setup_airflow.sh
 bash tools/setup_app.sh
+bash tools/install_python2_dependencies.sh
---
+++ tools/install_python2_dependencies.sh
@@ -0,0 +1,2 @@
+pip2 install soundfile
+pip2 install scipy==1.2.0
