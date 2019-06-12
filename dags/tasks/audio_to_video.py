fa6ca148f530e86b0cdcf380afc104b75e192fc4
shubham-padia <shubhamapadia@gmail.com>
2019-06-12 13:42:49 +0800

    Add dependency and fix acc strict setting

--- dags/tasks/audio_to_video.py
+++ dags/tasks/audio_to_video.py
@@ -13,7 +13,7 @@ def merge_audio(wav_list, output_file_prefix, video_dir):
 def convert_to_video(input_audio_path, output_file_prefix, video_dir):
     output_file_path = "%s/%s.mp4" % (video_dir, output_file_prefix)
     conversion_command = ["ffmpeg", "-loop", "1", "-i", "dags/tasks/video_conversion_black.jpg", "-i", input_audio_path, "-c:v",
-                          "libx264", "-c:a", "aac", "-b:a", "192k", "-pix_fmt", "yuv420p", "-shortest", output_file_path]
+                          "libx264", "-c:a", "aac", "-strict", "-2", "-b:a", "192k", "-pix_fmt", "yuv420p", "-shortest", output_file_path]
     
     subprocess.check_call(conversion_command)
 
